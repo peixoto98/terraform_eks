b6ef963532b3032b75f4239d15b3d2cda3414ed3
rp91058 <rp91058@saqueepague.com.br>
2023-02-08 13:21:22 -0300

    refactor: terraform modules

---
+++ modules/network/variables.tf
@@ -0,0 +1,14 @@
+variable "cidr_block" {
+    type = string
+    description = "Networking CIDR block to be used for the VPC"
+}
+
+variable "project_name" {
+    type = string
+    description = "Project name to be used to name the resources (Name Tag)"
+}
+
+variable "tags" {
+    type = map
+    description = "Tags to be add to AWS Resources"
+}
