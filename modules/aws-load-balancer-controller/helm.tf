540704fbf133919c954a1359402980f82e333c39
rp91058 <rp91058@saqueepague.com.br>
2023-02-15 09:00:38 -0300

    fix: fixing set clustername

--- modules/aws-load-balancer-controller/helm.tf
+++ modules/aws-load-balancer-controller/helm.tf
@@ -6,7 +6,7 @@ resource "helm_release" "eks_helm_controller" {
     namespace = "kube-system"
 
     set {
-        name = "cluster_name"
+        name = "clusterName"
         value = var.cluster_name
     }
 
